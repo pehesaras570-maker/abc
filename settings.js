9dd397f515b39c91b698b60cf46a658b64071e01
pehesaras570-maker <pehesaras570@gmail.com>
2025-10-26 13:02:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,4 +8,4 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
-};
+}; 
